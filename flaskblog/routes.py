64d961a76c2641f5056475eca3205c687ed8787f
bethanhutt <bethan157@hotmail.com>
2020-09-06 14:55:27 +0100

    user authentification

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,13 @@
 from flask import flash
 from flask import url_for
+from flask import request
 from flask import redirect
 from flask import render_template
+
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import current_user
+from flask_login import login_required
 
 from flaskblog import app
 from flaskblog import db
@@ -43,6 +49,9 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
 
     if form.validate_on_submit():
@@ -60,13 +69,33 @@ def register():
 
 @ app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+
         else:
-            flash('Login Unsuccessful.  Please check username and password', 'danger')
+            flash('Login Unsuccessful.  Please check email and password', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+
+@ app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@ app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
