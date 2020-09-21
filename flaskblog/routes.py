d02dd47cad24e32fde8f1b26ac7e59e5ed99a7e4
bethanhutt <bethan157@hotmail.com>
2020-09-21 18:02:22 +0100

    update and delete working

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,7 @@ import secrets
 
 from PIL import Image
 
+from flask import abort
 from flask import flash
 from flask import url_for
 from flask import request
@@ -128,7 +129,53 @@ def new_post():
         flash('Your post has been creaed!', 'success')
         return redirect(url_for('home'))
 
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if not post.author == current_user:
+        abort(403)
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated.', 'success')
+        return redirect(url_for('post', post_id=post.id))
+
+    if request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    if not post.author == current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    flash('Your post has been deleted.', 'success')
+    return redirect(url_for('home'))
 
 
 def save_picture(form_picture):
